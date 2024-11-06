a1bb8a96748e72ab3e3b6ece18ec35333734a74e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-05 19:26:47 -0600

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Isaac Urbina on 11/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
